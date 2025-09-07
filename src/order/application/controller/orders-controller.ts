114f2cecff7db9f85c1ce0fa723f51a3e074e01f
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-09-06 19:53:23 -0500

    Added basic part of order side

---
+++ src/order/application/controller/orders-controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response, NextFunction } from "express";
+import { OrderServiceContainer } from "../../infraestructure/order-service-container";
+import { IOrderRequest } from "../interfaces/orders";
+import {
+  orderCreatedStatus,
+  orderDeletedStatus,
+  orderUpdatedStatus,
+} from "../interfaces/order-response";
+
+//TODO: review the response of every one
+export class OrderController {
+  public async getById(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { id } = request.params;
+      const order = await OrderServiceContainer.getOrderById.run(Number(id));
+      return response.status(200).json(order);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async create(
+    request: Request<{}, {}, IOrderRequest>,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      await OrderServiceContainer.createOrder.run(request.body);
+      return response.status(200).json(orderCreatedStatus);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async update(
+    request: Request<{}, {}, IOrderRequest>,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      await OrderServiceContainer.createOrder.run(request.body);
+      return response.status(200).json(orderUpdatedStatus);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async delete(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { id } = request.params;
+      await OrderServiceContainer.deleteOrder.run(Number(id));
+      return response.status(200).json(orderDeletedStatus);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ src/order/domain/use-cases/update-order.user.case.ts
@@ -0,0 +1,10 @@
+import { IOrderRequest } from "../../application/interfaces/orders";
+
+export class UpdateOrder {
+  constructor() {}
+
+  async run(user: IOrderRequest): Promise<void> {
+
+    //TODO: save en database
+  }
+}
