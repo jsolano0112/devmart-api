6fc04d85796d6fb5db60b13ddaf65a9f51cf5008
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-09-21 19:39:06 -0500

    finish use cases of users

--- src/order/domain/use-cases/create-order.user.case.ts
+++ src/order/domain/use-cases/create-order.user.case.ts
@@ -5,6 +5,7 @@ export class CreateOrder {
   constructor(private repo: RepositoryContainer) {}
 
   async run(order: IOrder): Promise<void> {
+    await this.repo.users.getUserById(order.userId);
     await this.repo.orders.createOrder(order);
   }
 }
