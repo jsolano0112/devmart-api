1ccdfd7794558b543ad0ccbb50ddbd56e2f2929f
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-10-13 21:59:59 -0500

    add endpoint to cancel order

--- src/order/domain/use-cases/delete-order.use.case.ts
+++ src/order/domain/use-cases/delete-order.use.case.ts
@@ -1,10 +1,12 @@
+import { Exception } from '../../../shared/helpers/exception-message';
 import { RepositoryContainer } from '../../../shared/infraestructure/respository-container';
 
 export class DeleteOrder {
   constructor(private repo: RepositoryContainer) {}
 
-  async run(id: string): Promise<void> {
-    await this.repo.orders.getOrder(id);
+  async run(id: number): Promise<void> {
+    const order = await this.repo.orders.getOrder(id);
+    if(!order) throw new Exception('Order not found.', 404)
     await this.repo.orders.deleteOrder(id);
   }
 }
