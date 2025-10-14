936bde957998d49378f592909f2662320a5d0ce9
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-10-13 21:10:22 -0500

    Move logic to use case

--- src/order/domain/repositories/order.repository.ts
+++ src/order/domain/repositories/order.repository.ts
@@ -1,10 +1,4 @@
-import { Product } from '../../../product/domain/models/product.schema';
-import { Exception } from '../../../shared/helpers/exception-message';
-import {
-  IOrder,
-  IOrderResponse,
-  IUserOrderResponse,
-} from '../../../shared/interfaces/orders';
+import { IOrder, IOrderResponse } from '../../../shared/interfaces/orders';
 import { OrderSchema } from '../models/order.schema';
 
 export class OrderRepository {
@@ -36,74 +30,18 @@ export class OrderRepository {
     }
   }
 
-  public async getOrder(id: string): Promise<IOrderResponse> {
+  public async getOrder(id: number): Promise<IOrderResponse> {
     try {
-      const order = await OrderSchema.findById(id);
-      if (!order) throw new Exception('Order not found.', 404);
-
-      const { userId, products, paymentMethod, address, createdAt, updatedAt } =
-        order;
-
-      let total = 0;
-      for (const item of products) {
-        const product = await Product.findById(item.id);
-        if (product) {
-          total += product.price * item.count;
-        }
-      }
-
-      return {
-        id,
-        userId,
-        products,
-        paymentMethod,
-        address,
-        createdAt,
-        updatedAt,
-        total,
-      };
+      return await OrderSchema.findById(id);
     } catch (error) {
       console.error(error);
       throw error;
     }
   }
 
-  public async getOrders(userId: number): Promise<IUserOrderResponse[]> {
+  public async getOrders(userId: number) {
     try {
-      const orders = await OrderSchema.find({ userId }).lean();
-      const allProductIds = orders.flatMap((o) => o.products.map((p) => p.id));
-
-      // Search Products
-      const products = await Product.find({
-        _id: { $in: allProductIds },
-      }).lean();
-
-      // Mapping
-      return orders.map((order) => {
-        const count = order.products.reduce(
-          (acc: number, p: { count: number }) => acc + p.count,
-          0,
-        );
-
-        const total = order.products.reduce(
-          (sum: number, p: { id: string; count: number }) => {
-            const product = products.find(
-              (prod) => prod._id.toString() === p.id,
-            );
-            return product ? sum + product.price * p.count : sum;
-          },
-          0,
-        );
-
-        return {
-          id: order._id.toString(),
-          userId: order.userId.toString(),
-          count,
-          total,
-          createdAt: order.createdAt,
-          updatedAt: order.updatedAt,
-        };
-      });
+      return await OrderSchema.find({ userId }).lean();
     } catch (error) {
       console.error(error);
       throw error;
