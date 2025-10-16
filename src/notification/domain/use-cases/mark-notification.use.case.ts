4238e319bfa50b90ca937a8c2f80b51a2630e013
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-10-15 23:59:39 -0500

    Add exception

--- src/notification/domain/use-cases/mark-notification.use.case.ts
+++ src/notification/domain/use-cases/mark-notification.use.case.ts
@@ -1,3 +1,4 @@
+import { Exception } from '../../../shared/helpers/exception-message';
 import { RepositoryContainer } from '../../../shared/infraestructure/respository-container';
 
 export class MarkNotificationAsRead {
@@ -5,7 +6,7 @@ export class MarkNotificationAsRead {
 
   async run(id: string): Promise<void> {
     const notification = await this.repo.notifications.getById(id);
-    if (!notification) throw new Error('Notification not found');
+    if (!notification) throw new Exception('Notification not found', 404);
     notification.read = true;
     await this.repo.notifications.update(id, notification);
   }
