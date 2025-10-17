9a338e74dfed4eb44c1efab7cc091638962ca4be
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-10-16 22:11:40 -0500

    Fixes related to business logic

--- src/shared/helpers/scheduler/clean-pending-orders.ts
+++ src/shared/helpers/scheduler/clean-pending-orders.ts
@@ -1,16 +1,18 @@
 import cron from 'node-cron';
-import { RepositoryContainer } from '../../../shared/infraestructure/respository-container';
+import { RepositoryContainer } from '../../infraestructure/respository-container';
 import { CleanPendingOrders } from '../../../order/application/use-cases/clean-pending-orders.use.case';
+export class Scheduler {
+  private repo = new RepositoryContainer();
+  private cleanPendingOrders = new CleanPendingOrders(this.repo);
 
-const repo = new RepositoryContainer();
-const cleanPendingOrders = new CleanPendingOrders(repo);
-
-// per ten seconds
-cron.schedule('*/10 * * * * *', async () => {
-  console.log('JOB RUNNING: Cleaning pending orders.');
-  try {
-  await cleanPendingOrders.run();
-   } catch (err) {
-    console.error('JOB ERROR:', err);
+  start() {
+    cron.schedule('*/10 * * * * *', async () => {
+      console.log('JOB RUNNING: Cancel pending orders.');
+      try {
+        await this.cleanPendingOrders.run();
+      } catch (err) {
+        console.error('JOB ERROR:', err);
+      }
+    });
   }
-});
+}
