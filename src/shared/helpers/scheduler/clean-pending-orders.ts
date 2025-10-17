f240bb54bf8619bd42966dab3dc188849d79a07e
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-10-16 21:09:48 -0500

    move folders

--- src/shared/helpers/scheduler/clean-pending-orders.ts
+++ src/shared/helpers/scheduler/clean-pending-orders.ts
@@ -1,12 +1,16 @@
 import cron from 'node-cron';
 import { RepositoryContainer } from '../../../shared/infraestructure/respository-container';
-import { CleanPendingOrders } from '../../../order/domain/use-cases/clean-pending-orders.use.case';
+import { CleanPendingOrders } from '../../../order/application/use-cases/clean-pending-orders.use.case';
 
 const repo = new RepositoryContainer();
 const cleanPendingOrders = new CleanPendingOrders(repo);
 
-// per hour
-cron.schedule('0 * * * *', async () => {
+// per ten seconds
+cron.schedule('*/10 * * * * *', async () => {
   console.log('JOB RUNNING: Cleaning pending orders.');
+  try {
   await cleanPendingOrders.run();
+   } catch (err) {
+    console.error('JOB ERROR:', err);
+  }
 });
